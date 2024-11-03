7f1a7789e6ec804dcf5f09d0fc356e017abf8574
Arthur2479 <arthurjacobs1004@gmail.com>
2024-11-03 09:33:48 +0100

    Add command parser

--- __main__.py
+++ __main__.py
@@ -1,1 +1,23 @@
+import sys
+import argparse
+from groups_gestion.difference import difference
+
+def main():
+    # Parse arguments: command, file1, file2
+    parser = argparse.ArgumentParser(description='Process CSV files containing email lists.')
+    parser.add_argument('operation', type=str, help='Operation to perform (currently only "difference" is supported)')
+    parser.add_argument('file1', type=str, help='Path to first CSV file')
+    parser.add_argument('file2', type=str, help='Path to second CSV file')
+    
+    args = parser.parse_args()
+    
+    # Execute the operation. We only support difference for now.
+    if args.operation.lower() == "difference":
+        # TODO
+    else:
+        print(f"Unsupported operation: {args.operation}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
 
